175c7e7da2a5612dbdaab76b2690bc9a2b7bf2a8
Mark <menachemsalzberg13@gmail.com>
2025-09-01 16:50:47 +0300

    Add layout prop to MembersList component for dynamic rendering

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -2,7 +2,10 @@ import "../styles/App.css";
 import type IMemberCard from "../interfaces/ImemberCard.ts";
 import MemberCard from "./MemberCard.tsx";
 
-export default function MembersList(props: { members: IMemberCard[] }) {
+export default function MembersList(props: {
+  members: IMemberCard[];
+  layout: string;
+}) {
   return (
     <div className="members-list">
       {props.members.map((member, idx) => (
